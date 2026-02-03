853ebebd2e36ad67d2c9f9ad06452a85e8141ab9
Joshua Nance <joshua.h.nance@gmail.com>
2026-02-03 10:36:21 -0800

    Remove tests

--- arrays/two-sum.js
+++ arrays/two-sum.js
@@ -26,7 +26,7 @@
  * Output: [0, 1]
  */
 
-function twoSum(nums, target) {
+export function twoSum(nums, target) {
     const encountered = new Map()
 
     for (let [index, num] of nums.entries()) {
@@ -39,48 +39,3 @@ function twoSum(nums, target) {
         encountered.set(num, index)
     }
 }
-
-function assertEquals(a, b) {
-    return a[0] === b[0] && a[1] === b[1]
-}
-
-const tests = [
-    {
-        name: 'Test one',
-        execute: () => {
-            const nums = [2, 7, 11, 15]
-            const result = twoSum(nums, 9)
-            return assertEquals(result, [0, 1]) || assertEquals(result, [1, 0])
-        }
-    },
-    {
-        name: 'Test two',
-        execute: () => {
-            const nums = [3, 2, 4]
-            const result = twoSum(nums, 6)
-            return assertEquals(result, [1, 2]) || assertEquals(result, [2, 1])
-        }
-    },
-    {
-        name: 'Test three',
-        execute: () => {
-            const nums = [3, 3]
-            const result = twoSum(nums, 6)
-            return assertEquals(result, [0, 1]) || assertEquals(result, [1, 0])
-        }
-    }
-]
-
-function runTests() {
-    const passed = []
-    const failed = []
-
-    tests.forEach(test => {
-        test.execute() ? passed.push(test.name) : failed.push(test.name)
-    })
-
-    passed.forEach(p => console.log(p + ' ✓'))
-    failed.forEach(f => console.error(f + ' ✖'))
-}
-
-runTests()
