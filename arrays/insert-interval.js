3bee6c39bfb9dd76054c1e6f9d0840386fff09ef
Joshua Nance <joshua.h.nance@gmail.com>
2026-04-30 13:14:12 -0700

    Add solution and tests for insert interval

---
+++ arrays/insert-interval.js
@@ -0,0 +1,76 @@
+/**
+ * Given a sorted list of non-overlapping `intervals` and an interval `newInterval`,
+ * write a function to insert `newInterval` into that list, ensuring it remains sorted
+ * based on the intervals' start times.
+ */
+
+const START = 0
+const END = 1
+
+export function insertInterval(intervals, newInterval) {
+    const merged = []
+    let i = 0
+    const n = intervals.length
+
+    // insert intervals as long as they don't conflict with the new one
+    while (i < n && intervals[i][END] < newInterval[START]) {
+        merged.push(intervals[i])
+        i++
+    }
+
+    /**
+     * We break out of the first loop as soon as we encounter the first interval
+     * that conflicts with the newInterval.
+     *
+     * We don't need to combine anything to the left of i, because we already know
+     * that everything prior to intervals[i] ends both before intervals[i] and newInterval,
+     * so there's nothing to merge.
+     *
+     * We only need to look forward, and expand newInterval until its end does not
+     * conflict with the next interval in our original list.
+     */
+    while (i < n && intervals[i][START] <= newInterval[END]) {
+        newInterval[START] = Math.min(intervals[i][START], newInterval[START])
+        newInterval[END] = Math.max(intervals[i][END], newInterval[END])
+        i++
+    }
+    merged.push(newInterval)
+
+    /**
+     * All that's left to do is push the remaining intervals, because we know
+     * they can't possibly conflict with the merged `newInterval`.
+     */
+    for (i; i < n; i++) {
+        merged.push(intervals[i])
+    }
+
+    return merged
+}
+
+/**
+ * This is the naive approach. It just treats this as one large, regular merge overlapping
+ * intervals operation after inserting the new interval and re-sorting.
+ *
+ * There's a faster way above.
+ */
+function _insertInterval(intervals, newInterval) {
+    const sortedIntervals = [...intervals, newInterval]
+        .map((i) => [...i])
+        .sort((a, b) => a.at(START) - b.at(START))
+
+    const merged = [sortedIntervals.at(0)]
+    let prev = merged.at(0)
+
+    for (let i = 1; i < sortedIntervals.length; i++) {
+        const curr = sortedIntervals.at(i)
+
+        if (curr.at(START) > prev.at(END)) {
+            prev = curr
+            merged.push(curr)
+        } else {
+            prev[END] = Math.max(prev.at(END), curr.at(END))
+        }
+    }
+
+    return merged
+}
---
+++ arrays/insert-interval.test.js
@@ -0,0 +1,88 @@
+import { insertInterval } from './insert-interval'
+
+test('inserts interval at the beginning, no merging', () => {
+    expect(
+        insertInterval(
+            [
+                [2, 3],
+                [4, 5],
+            ],
+            [0, 1],
+        ),
+    ).toEqual([
+        [0, 1],
+        [2, 3],
+        [4, 5],
+    ])
+})
+
+test('inserts interval in the middle, no merging', () => {
+    expect(
+        insertInterval(
+            [
+                [2, 3],
+                [6, 7],
+            ],
+            [4, 5],
+        ),
+    ).toEqual([
+        [2, 3],
+        [4, 5],
+        [6, 7],
+    ])
+})
+
+test('inserts interval at the end', () => {
+    expect(
+        insertInterval(
+            [
+                [2, 3],
+                [4, 5],
+            ],
+            [6, 7],
+        ),
+    ).toEqual([
+        [2, 3],
+        [4, 5],
+        [6, 7],
+    ])
+})
+
+test('correctly merges intervals after insertion', () => {
+    // merges from the beginning
+    expect(
+        insertInterval(
+            [
+                [2, 3],
+                [4, 5],
+            ],
+            [0, 2],
+        ),
+        'inserting into the beginning of the original list when there are merge opportunities',
+    ).toEqual([
+        [0, 3],
+        [4, 5],
+    ])
+
+    // merges from the middle
+    expect(
+        insertInterval(
+            [
+                [2, 3],
+                [4, 5],
+                [7, 9],
+                [10, 11],
+            ],
+            [4, 8],
+        ),
+        'inserting into the middle of the original list when there are merge opportunities',
+    ).toEqual([
+        [2, 3],
+        [4, 9],
+        [10, 11],
+    ])
+})
+
+test('inserting a newInterval into an empty list of intervals returns a list containing only newInterval', () => {
+    expect(insertInterval([], [1, 2])).toEqual([[1, 2]])
+})
