c222c4059fd135982faa03748dfbe057f9fcf959
Joshua Nance <joshua.h.nance@gmail.com>
2026-04-30 10:38:47 -0700

    Add more edge cases to test Merge Overlapping Intervals

--- arrays/merge-overlapping-intervals.js
+++ arrays/merge-overlapping-intervals.js
@@ -1,19 +1,42 @@
-const START = 0
-const END = 1
-
+/**
+ * Assumptions
+ * - You cannot have a zero-length interval
+ * - Either or both of the input arrays can be empty
+ *
+ * Constraints:
+ * - If previous endTime is 1 and current startTime is 1, do not merge
+ *
+ * Extra notes:
+ * - While it would seem strange to have a single machine's list contain overlapping intervals (meaning the machine
+ *   was both on and off at the same time), this algorithm will still handle that merge. I don't know if this specifically
+ *   is part of the "chain merge" case that we discussed in the interview. It will handle this kind of chain or the one
+ *   that seems more likely, where you're chaining events across the two machines.
+ */
 export function mergeIntervals(a, b) {
-    const sortedIntervals = [...a, ...b].sort(
-        (a, b) => a.at(START) - b.at(START),
-    )
+    const sortedIntervals = [...a, ...b]
+        .map((interval) => ({ ...interval }))
+        .sort((a, b) => a.startTime - b.startTime)
     const merged = []
 
     for (const current of sortedIntervals) {
         const previous = merged.at(-1)
 
-        if (!previous || current.at(START) >= previous.at(END)) {
+        /**
+         * If I remember correctly, one of the clarifications made in the interview
+         * was that we should not treat two intervals such as these as overlapping:
+         * Machine A: {startTime: 0, endTime: 1}
+         * Machine B: {startTime: 1, endTime: 2}
+         *
+         * Although A's endTime is equal to B's startTime, we want to treat them
+         * as separate.
+         *
+         * If that's not the case then we just change our check below to:
+         * current.startTime > previous.endTime.
+         */
+        if (!previous || current.startTime >= previous.endTime) {
             merged.push(current)
         } else {
-            previous[END] = Math.max(previous.at(END), current.at(END))
+            previous.endTime = Math.max(previous.endTime, current.endTime)
         }
     }
 
--- arrays/merge-overlapping-intervals.test.js
+++ arrays/merge-overlapping-intervals.test.js
@@ -2,68 +2,190 @@ import { mergeIntervals } from './merge-overlapping-intervals'
 
 test('merges overlapping intervals', () => {
     const intervalSets = [
-        // first set
+        // unsorted inputs
         {
             intervals: [
                 [
-                    [1, 4],
-                    [8, 19],
+                    { startTime: 8, endTime: 19, name: 'a' },
+                    { startTime: 1, endTime: 4, name: 'a' },
                 ],
                 [
-                    [0, 2],
-                    [6, 8],
-                    [11, 15],
-                    [21, 23],
-                    [25, 29],
+                    { startTime: 25, endTime: 29, name: 'b' },
+                    { startTime: 6, endTime: 8, name: 'b' },
+                    { startTime: 0, endTime: 2, name: 'b' },
+                    { startTime: 21, endTime: 23, name: 'b' },
+                    { startTime: 11, endTime: 15, name: 'b' },
                 ],
             ],
             expected: [
-                [0, 4],
-                [6, 8],
-                [8, 19],
-                [21, 23],
-                [25, 29],
+                { startTime: 0, endTime: 4 },
+                { startTime: 6, endTime: 8 },
+                { startTime: 8, endTime: 19 },
+                { startTime: 21, endTime: 23 },
+                { startTime: 25, endTime: 29 },
+            ],
+        },
+        // basic overlaps
+        {
+            intervals: [
+                [
+                    { startTime: 1, endTime: 4, name: 'a' },
+                    { startTime: 8, endTime: 19, name: 'a' },
+                ],
+                [
+                    { startTime: 0, endTime: 2, name: 'b' },
+                    { startTime: 6, endTime: 8, name: 'b' },
+                    { startTime: 11, endTime: 15, name: 'b' },
+                    { startTime: 21, endTime: 23, name: 'b' },
+                    { startTime: 25, endTime: 29, name: 'b' },
+                ],
+            ],
+            expected: [
+                { startTime: 0, endTime: 4 },
+                { startTime: 6, endTime: 8 },
+                { startTime: 8, endTime: 19 },
+                { startTime: 21, endTime: 23 },
+                { startTime: 25, endTime: 29 },
             ],
         },
 
-        // second set
+        // first has only one interval
         {
             intervals: [
+                [{ startTime: 1, endTime: 4, name: 'a' }],
                 [
-                    [0, 2],
-                    [7, 11],
-                ],
-                [
-                    [1, 5],
-                    [6, 8],
-                    [10, 12],
+                    { startTime: 0, endTime: 2, name: 'b' },
+                    { startTime: 6, endTime: 8, name: 'b' },
+                    { startTime: 11, endTime: 15, name: 'b' },
+                    { startTime: 21, endTime: 23, name: 'b' },
+                    { startTime: 25, endTime: 29, name: 'b' },
                 ],
             ],
             expected: [
-                [0, 5],
-                [6, 12],
+                { startTime: 0, endTime: 4 },
+                { startTime: 6, endTime: 8 },
+                { startTime: 11, endTime: 15 },
+                { startTime: 21, endTime: 23 },
+                { startTime: 25, endTime: 29 },
             ],
         },
 
-        // third set, identical intervals
+        // second has only one interval
         {
             intervals: [
                 [
-                    [0, 5],
-                    [6, 10],
-                    [11, 15],
+                    { startTime: 0, endTime: 2, name: 'a' },
+                    { startTime: 6, endTime: 8, name: 'a' },
+                    { startTime: 11, endTime: 15, name: 'a' },
+                    { startTime: 21, endTime: 23, name: 'a' },
+                    { startTime: 25, endTime: 29, name: 'a' },
+                ],
+
+                [{ startTime: 1, endTime: 4, name: 'b' }],
+            ],
+            expected: [
+                { startTime: 0, endTime: 4 },
+                { startTime: 6, endTime: 8 },
+                { startTime: 11, endTime: 15 },
+                { startTime: 21, endTime: 23 },
+                { startTime: 25, endTime: 29 },
+            ],
+        },
+
+        // first interval fully subsumes the second
+        {
+            intervals: [
+                [{ startTime: 0, endTime: 10, name: 'a' }],
+                [{ startTime: 1, endTime: 5, name: 'b' }],
+            ],
+            expected: [{ startTime: 0, endTime: 10 }],
+        },
+
+        // second interval fully subsumes the first
+        {
+            intervals: [
+                [{ startTime: 1, endTime: 5, name: 'a' }],
+                [{ startTime: 0, endTime: 10, name: 'b' }],
+            ],
+            expected: [{ startTime: 0, endTime: 10 }],
+        },
+
+        // identical intervals
+        {
+            intervals: [
+                [
+                    { startTime: 0, endTime: 5, name: 'a' },
+                    { startTime: 6, endTime: 10, name: 'a' },
+                    { startTime: 11, endTime: 15, name: 'a' },
                 ],
                 [
-                    [0, 5],
-                    [6, 10],
-                    [11, 15],
+                    { startTime: 0, endTime: 5, name: 'b' },
+                    { startTime: 6, endTime: 10, name: 'b' },
+                    { startTime: 11, endTime: 15, name: 'b' },
                 ],
             ],
             expected: [
-                [0, 5],
-                [6, 10],
-                [11, 15],
+                { startTime: 0, endTime: 5 },
+                { startTime: 6, endTime: 10 },
+                { startTime: 11, endTime: 15 },
             ],
+        },
+
+        // all of Machine A's intervals come before B's
+        {
+            intervals: [
+                [
+                    { startTime: 0, endTime: 5, name: 'a' },
+                    { startTime: 11, endTime: 15, name: 'a' },
+                ],
+                [{ startTime: 16, endTime: 17, name: 'b' }],
+            ],
+            expected: [
+                { startTime: 0, endTime: 5 },
+                { startTime: 11, endTime: 15 },
+                { startTime: 16, endTime: 17 },
+            ],
+        },
+
+        // all of Machine B's intervals come before A's
+        {
+            intervals: [
+                [{ startTime: 16, endTime: 17, name: 'b' }],
+                [
+                    { startTime: 0, endTime: 5, name: 'a' },
+                    { startTime: 11, endTime: 15, name: 'a' },
+                ],
+            ],
+            expected: [
+                { startTime: 0, endTime: 5 },
+                { startTime: 11, endTime: 15 },
+                { startTime: 16, endTime: 17 },
+            ],
+        },
+
+        // chain merges, across machines
+        {
+            intervals: [
+                [
+                    { startTime: 0, endTime: 3, name: 'a' },
+                    { startTime: 4, endTime: 8, name: 'a' },
+                ],
+                [{ startTime: 2, endTime: 5, name: 'b' }],
+            ],
+            expected: [{ startTime: 0, endTime: 8 }],
+        },
+
+        // chain merges, overlapping within a single machine's list
+        // this one seems unlikely, but it will still be handled
+        {
+            intervals: [
+                [{ startTime: 0, endTime: 3, name: 'a' }],
+                [
+                    { startTime: 2, endTime: 5, name: 'b' },
+                    { startTime: 4, endTime: 8, name: 'b' },
+                ],
+            ],
+            expected: [{ startTime: 0, endTime: 8 }],
         },
     ]
 
@@ -71,69 +193,88 @@ test('merges overlapping intervals', () => {
         const [a, b] = intervals
         const actual = mergeIntervals(a, b)
 
-        expect(actual).toEqual(expected)
+        expect(
+            actual.map(({ startTime, endTime }) => ({ startTime, endTime })),
+        ).toEqual(expected)
     }
 })
 
 test('handles empty lists', () => {
     const intervalSets = [
-        // first set, empty list in first position
+        // first list is empty
         {
-            intervals: [[], [[0, 2]]],
-            expected: [[0, 2]],
+            intervals: [[], [{ startTime: 0, endTime: 2, name: 'b' }]],
+            expected: [{ startTime: 0, endTime: 2 }],
         },
-        // second set, empty list in second position
+        // second list is empty
         {
-            intervals: [[[0, 2]], []],
-            expected: [[0, 2]],
+            intervals: [[{ startTime: 0, endTime: 2, name: 'a' }], []],
+            expected: [{ startTime: 0, endTime: 2 }],
+        },
+        // both lists are empty
+        {
+            intervals: [[], []],
+            expected: [],
         },
     ]
 
     for (const { intervals, expected } of intervalSets) {
         const [a, b] = intervals
         const actual = mergeIntervals(a, b)
-        expect(actual).toEqual(expected)
+        expect(
+            actual.map(({ startTime, endTime }) => ({ startTime, endTime })),
+        ).toEqual(expected)
     }
 })
 
+/**
+ * This test assumes the following intervals do not overlap, despite A's endTime
+ * matching B's startTime:
+ *
+ * Machine A: {startTime: 0, endTime: 1}
+ * Machine B: {startTime: 1, endTime: 2}
+ *
+ * There's a note in the implementation to the same effect; it's a simple change
+ * if we want to accommodate a merge in this instance.
+ * */
 test('does not merge non-overlapping intervals', () => {
     const intervalSets = [
-        // first set, no startTime === previous endTime
+        // no overlap of one machine's startTime with another's endTime
         {
             intervals: [
                 [
-                    [0, 1],
-                    [2, 3],
+                    { startTime: 0, endTime: 1, name: 'a' },
+                    { startTime: 2, endTime: 3, name: 'a' },
                 ],
                 [
-                    [4, 5],
-                    [6, 7],
+                    { startTime: 4, endTime: 5, name: 'b' },
+                    { startTime: 6, endTime: 7, name: 'b' },
                 ],
             ],
             expected: [
-                [0, 1],
-                [2, 3],
-                [4, 5],
-                [6, 7],
+                { startTime: 0, endTime: 1 },
+                { startTime: 2, endTime: 3 },
+                { startTime: 4, endTime: 5 },
+                { startTime: 6, endTime: 7 },
             ],
         },
-        // second set, startTime and endTime overlap
+        // startTime and endTime overlap across machines
         {
             intervals: [
                 [
-                    [0, 1],
-                    [1, 2],
+                    { startTime: 0, endTime: 1, name: 'a' },
+                    { startTime: 1, endTime: 2, name: 'a' },
                 ],
                 [
-                    [2, 3],
-                    [3, 4],
+                    { startTime: 2, endTime: 3, name: 'b' },
+                    { startTime: 3, endTime: 4, name: 'b' },
                 ],
             ],
             expected: [
-                [0, 1],
-                [1, 2],
-                [2, 3],
-                [3, 4],
+                { startTime: 0, endTime: 1 },
+                { startTime: 1, endTime: 2 },
+                { startTime: 2, endTime: 3 },
+                { startTime: 3, endTime: 4 },
             ],
         },
     ]
@@ -141,6 +282,30 @@ test('does not merge non-overlapping intervals', () => {
     for (const { intervals, expected } of intervalSets) {
         const [a, b] = intervals
         const actual = mergeIntervals(a, b)
-        expect(actual).toEqual(expected)
+        expect(
+            actual.map(({ startTime, endTime }) => ({ startTime, endTime })),
+        ).toEqual(expected)
     }
+})
+
+test('does not mutate the inputs', () => {
+    const originalMachineAList = [
+        { startTime: 8, endTime: 19, name: 'a' },
+        { startTime: 1, endTime: 4, name: 'a' },
+    ]
+    const originalMachineBList = [
+        { startTime: 25, endTime: 29, name: 'b' },
+        { startTime: 6, endTime: 8, name: 'b' },
+        { startTime: 0, endTime: 2, name: 'b' },
+        { startTime: 21, endTime: 23, name: 'b' },
+        { startTime: 11, endTime: 15, name: 'b' },
+    ]
+
+    const expectedA = structuredClone(originalMachineAList)
+    const expectedB = structuredClone(originalMachineBList)
+
+    mergeIntervals(originalMachineAList, originalMachineBList)
+
+    expect(originalMachineAList).toEqual(expectedA)
+    expect(originalMachineBList).toEqual(expectedB)
 })
