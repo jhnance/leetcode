3136c4f1dd2ddfbc6730b3132d481567097cec6c
Joshua Nance <joshua.h.nance@gmail.com>
2026-04-30 12:12:59 -0700

    Update comments

--- arrays/non-overlapping-intervals.js
+++ arrays/non-overlapping-intervals.js
@@ -38,3 +38,34 @@ export function nonOverlappingIntervals(intervals) {
 
     return count
 }
+
+/**
+ * This is an alternative approach to the above. Given the framing of the problem (count minimum removals) I find
+ * the above approach easier to think through. But this makes sense too; you just flip it on its head to identify
+ * the actual pattern at play here, which is what the name of the functions suggest anyway.
+ *
+ * Differences:
+ * - Starts the count at 1 and counts the opposite of ours: here we're not tracking number of removals, we're
+ *   counting the number of non-overlapping intervals. By definition, nothing overlaps with the first, since we sorted
+ *   by end time and nothing ends before it.
+ */
+export function _nonOverlappingIntervals(intervals) {
+    if (intervals.length === 0) return 0
+
+    const sortedIntervals = intervals
+        .map((i) => [...i])
+        .sort((a, b) => a.at(END) - b.at(END))
+
+    let prevEnd = sortedIntervals.at(START).at(END)
+    let count = 1
+
+    for (let i = 1; i < sortedIntervals.length; i++) {
+        const curr = sortedIntervals.at(i)
+        if (curr.at(START) >= prevEnd) {
+            count++
+            prevEnd = curr.at(END)
+        }
+    }
+
+    return sortedIntervals.length - count
+}
