cc1d4cb217aff7fb0e8bad5f86034c08d2a955a9
Joshua Nance <joshua.h.nance@gmail.com>
2026-05-14 07:58:57 -0700

    Add comment explaining longest repeating character replacement

--- sliding-window/longest-repeating-character-replacement.js
+++ sliding-window/longest-repeating-character-replacement.js
@@ -1,3 +1,18 @@
+/**
+ * The crux of this solution is the conditional in the loop:
+ * if (end - start + 1 - maxFrequency > k) is doing all the work here.
+ *
+ * end - start + 1 is the length of the substring we're considering.
+ * If we subtract from the substring the maximum frequency character, that
+ * should give us the number of replacements we can still make.
+ *
+ * But, what if we don't have our most frequent character in this string?
+ * It doesn't matter! Because we know that our maxFrequency gives us the longest
+ * substring before making replacements, and the goal is to find the longest substring.
+ *
+ * Any lower frequency isn't worth considering again once we reach each maxFrequency
+ * threshold.
+ */
 function characterReplacement(s, k) {
     let visited = {}
     let maxFrequency = 0
