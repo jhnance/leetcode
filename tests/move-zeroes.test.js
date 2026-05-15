26006649861f40b6aef828969799309a75db67a4
Joshua Nance <joshua.h.nance@gmail.com>
2026-05-15 12:44:31 -0700

    Remove old Move Zeroes solution and tests

--- tests/move-zeroes.test.js
+++
@@ -1,8 +0,0 @@
-import { moveZeroes } from '../arrays/move-zeroes'
-
-test('Returns a new array with the zeroes at the end, where the non-zero values keep their original relative ordering', () => {
-    expect(moveZeroes([0, 1, 0, 3, 12])).toEqual([1, 3, 12, 0, 0])
-    expect(moveZeroes([0, 0, 0, 3, 12])).toEqual([3, 12, 0, 0, 0])
-    expect(moveZeroes([1, 3, 12, 0, 0])).toEqual([1, 3, 12, 0, 0])
-    expect(moveZeroes([1, 3, 12])).toEqual([1, 3, 12])
-})
