b67a8e05eb6ef9a758b3d6959278b0e5c7d70974
Joshua Nance <joshua.h.nance@gmail.com>
2026-05-16 19:59:16 -0700

    Add solution to Count Good Nodes in a Binary Tree

---
+++ dfs/good-nodes.js
@@ -0,0 +1,46 @@
+/**
+ * Difficulty: medium
+ * Leetcode link: https://leetcode.com/problems/count-good-nodes-in-binary-tree
+ *
+ * Description
+ * -----------
+ * Given a binary tree root, a node X in the tree is named good if in the path
+ * from root to X there are no nodes with a value greater than X.
+ *
+ * Return the number of good nodes in the binary tree.
+ *
+ * Examples
+ * --------
+ * Input: root = [3,1,4,3,null,1,5]
+ * Output: 4
+ * Explanation: Nodes in blue are good.
+ * Root Node (3) is always a good node.
+ * Node 4 -> (3,4) is the maximum value in the path starting from the root.
+ * Node 5 -> (3,4,5) is the maximum value in the path
+ * Node 3 -> (3,1,3) is the maximum value in the path.
+ *
+ * Input: root = [3,3,null,4,2]
+ * Output: 3
+ * Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+ *
+ * Input: root = [1]
+ * Output: 1
+ * Explanation: Root is considered as good.
+ */
+function goodNodes(root) {
+    function countGoodNodes(root, max) {
+        if (root === null) return 0
+
+        let count = 0
+        if (root.val >= max) {
+            count++
+        }
+
+        const newMax = Math.max(root.val, max)
+        const left = countGoodNodes(root.left, newMax)
+        const right = countGoodNodes(root.right, newMax)
+
+        return count + left + right
+    }
+    return countGoodNodes(root, root.val)
+}
